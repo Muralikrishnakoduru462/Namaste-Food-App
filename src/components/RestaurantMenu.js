e7323e0983a78d594ad75d6988fb03375c2f9533
Muralikrishnakoduru462 <muralikrishnakoduru462@gmail>
2024-09-21 18:28:10 +0530

    importChanged

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,7 +1,7 @@
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
-import useRestaurantMenu from "../utils/useRestaurantmenu";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
@@ -11,12 +11,17 @@ const RestaurantMenu = () => {
   const resInfo = useRestaurantMenu(resId);
 
   if (resInfo === null) return <Shimmer />;
-  const { name, cuisines, costForTwoMessage } = resInfo?.cards[2]?.card?.card?.info;
-  const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[2]?.card?.card?.info;
+  const { itemCards } =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
 
-
-
-  const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(c => c?.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
+  const categories =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c?.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
 
   return (
     <div className="text-center">
@@ -24,9 +29,15 @@ const RestaurantMenu = () => {
       <p className="font-bold text-lg">
         {cuisines.join(",")}-{costForTwoMessage}
       </p>
-      {categories.map((category, index) => <RestaurantCategory key={category?.card?.card?.title} data={category?.card?.card} showItems={index === showIndex ? true : false} setShowindex={() => setShowindex(index)} />)}
-
+      {categories.map((category, index) => (
+        <RestaurantCategory
+          key={category?.card?.card?.title}
+          data={category?.card?.card}
+          showItems={index === showIndex ? true : false}
+          setShowindex={() => setShowindex(index)}
+        />
+      ))}
     </div>
-  )
+  );
 };
 export default RestaurantMenu;
